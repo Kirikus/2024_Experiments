5f51f81f6d82e9b75a1ce4b223d7015cba0c54e6
Kirikus <kirikusan@gmail.com>
2024-02-02 14:40:51 +0300

    tests: added numerical tests for water vapour

--- tests/test_module.cpp
+++ tests/test_module.cpp
@@ -8,3 +8,17 @@
 #include <boost/test/unit_test.hpp>
 
 namespace tt = boost::test_tools;
+
+BOOST_AUTO_TEST_SUITE(test_rho_w)
+
+BOOST_AUTO_TEST_CASE(ground_level) { BOOST_TEST(rho_w(0, 0.5, 42) == 21); }
+
+BOOST_AUTO_TEST_CASE(rho_w_2000m) {
+  BOOST_TEST(rho_w(2000 - 1e-6) == rho_w(2000 + 1e-6), tt::tolerance(1e-6));
+}
+
+BOOST_AUTO_TEST_CASE(rho_w_8000m) {
+  BOOST_TEST(rho_w(8000 - 1e-6) == rho_w(8000 + 1e-6), tt::tolerance(1e-6));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
