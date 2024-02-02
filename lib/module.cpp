8ad8d10a4b0410f68d5bd1f922276f529f5e883a
Kirikus <kirikusan@gmail.com>
2024-02-02 14:30:20 +0300

    core: added water vapour density function

--- lib/module.cpp
+++ lib/module.cpp
@@ -1,1 +1,21 @@
 #include "module.h"
+
+#include <cmath>
+
+double rho_w(double h, double rh, double pho_w_max) {
+  const double pho_w0 = pho_w_max * rh;
+  const double h_1 = 2000;
+  const double h_2 = 8000;
+  const double slope_low = 0.2523 / 1000;
+  const double rho_w_h_1 = (1 - slope_low * h_1);
+  const double slope_mid = 1861;
+  const double rho_w_h_2 = rho_w_h_1 * std::exp(-(h_2 - 2000) / slope_mid);
+  const double slope_high = 1158;
+
+  if (h < h_1)
+    return pho_w0 * (1 - slope_low * h);
+  else if (h < h_2)
+    return pho_w0 * rho_w_h_1 * std::exp(-(h - h_1) / slope_mid);
+  else
+    return pho_w0 * rho_w_h_2 * std::exp(-(h - h_2) / slope_high);
+}
--- lib/module.h
+++ lib/module.h
@@ -1,4 +1,12 @@
 #ifndef MODULE_H
 #define MODULE_H
 
+// Get water vapor density on specified height.
+// SRC: (2.25) from citation
+//   rho_w : water vapor density, g/m**3
+//   h : height above sea level, m
+//   rh : relative humidity, n/d
+//   pho_w_max : saturated vapor density, g/m**3
+double rho_w(double h, double rh = 0.6, double pho_w_max = 12.8 * 0.001);
+
 #endif  // MODULE_H
