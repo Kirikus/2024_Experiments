923aca275dd90163fd841cdeee07467faa91bf0c
TheFueR <sukhanov.ge@phystech.edu>
2024-03-02 21:49:07 +0300

    add measurements_table files

---
+++ main/measurements_table.h
@@ -0,0 +1,10 @@
+#ifndef MEASUREMENTS_TABLE_H
+#define MEASUREMENTS_TABLE_H
+
+#include <QAbstractTableModel>
+
+class MeasurementsTable: QAbstractTableModel {
+
+};
+
+#endif // MEASUREMENTS_TABLE_H
