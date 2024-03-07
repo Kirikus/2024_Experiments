f6bc230c686a4871004d748cb9191a1e5604ff8c
TheFueR <sukhanov.ge@phystech.edu>
2024-03-07 18:49:04 +0300

    Created the MeasurementsTable class - the inheritor of QAbstractTabelModel. Implemented in it the possibilities of displaying and editing Manager Measurements.

--- main/measurements_table.h
+++ main/measurements_table.h
@@ -3,8 +3,25 @@
 
 #include <QAbstractTableModel>
 
-class MeasurementsTable: QAbstractTableModel {
+namespace lib {
 
+class MeasurementsTable : public QAbstractTableModel {
+  Q_OBJECT
+ public:
+  explicit MeasurementsTable(QObject *parent = nullptr);
+
+  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+  int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+  QVariant data(const QModelIndex &index,
+                int role = Qt::DisplayRole) const override;
+  bool setData(const QModelIndex &index, const QVariant &value,
+               int role = Qt::EditRole) override;
+  Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+  void insertRow(int row, const QModelIndex &parent);
+  void insertColumn(int column, const QModelIndex &parent);
 };
 
-#endif // MEASUREMENTS_TABLE_H
+}  // namespace lib
+
+#endif  // MEASUREMENTS_TABLE_H
