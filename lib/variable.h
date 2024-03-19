68a48ab140fe119ada96ff8e06be4369ba672f4e
TheFueR <sukhanov.ge@phystech.edu>
2024-03-19 15:58:18 +0300

    create visual options struct.

--- lib/variable.h
+++ lib/variable.h
@@ -1,17 +1,25 @@
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
+#include "QColor"
 #include "QList"
 
 namespace lib {
 
-// SimpleClass
-class Variable {
- public:
+struct VisualOptions {
+  bool visible = true;
+  int width = 1;
+  QColor color = "black";
+};
+
+struct Variable {
   QList<double> measurements;
   QString name_full = "NONE";
   QString name_short = "NONE";
+  VisualOptions variable_visual;
+
   size_t getMeasurementsCount() const { return measurements.size(); }
+
   Variable(QList<double> measurements, QString name_full = "NONE",
            QString name_short = "NONE");
   Variable();
