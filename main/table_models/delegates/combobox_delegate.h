493d3ebb1875da3c4bb1a400c69e42f4aa55838d
TheFueR <sukhanov.ge@phystech.edu>
2024-03-20 20:43:45 +0300

    realize combo box delegate

--- main/table_models/delegates/combobox_delegate.h
+++ main/table_models/delegates/combobox_delegate.h
@@ -1,4 +1,22 @@
 #ifndef COMBOBOX_DELEGATE_H
 #define COMBOBOX_DELEGATE_H
 
+#include <QStyledItemDelegate>
+
+class ComboBoxDelegate : public QStyledItemDelegate {
+  Q_OBJECT
+ public:
+  ComboBoxDelegate(QStringList options, QObject* parent = nullptr);
+  ~ComboBoxDelegate();
+
+  QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option,
+                        const QModelIndex& index) const override;
+  void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+  void setModelData(QWidget* editor, QAbstractItemModel* model,
+                    const QModelIndex& index) const override;
+
+ private:
+  QStringList options;
+};
+
 #endif  // COMBOBOX_DELEGATE_H
