886cc233f5e66439e15837c7043b445873e83de6
TheFueR <sukhanov.ge@phystech.edu>
2024-03-21 16:09:20 +0300

    open and read json file

--- lib/strategyIO.cpp
+++ lib/strategyIO.cpp
@@ -4,7 +4,16 @@
 
 void StrategyIO_JSON::save(const QString&) {}
 
-void StrategyIO_JSON::load(const QString&) {}
+void StrategyIO_JSON::load(const QString& input_file) {
+  QString data;
+  QFile file;
+  file.setFileName(input_file);
+  file.open(QIODevice::ReadOnly | QIODevice::Text);
+  data = file.readAll();
+  file.close();
+
+  QJsonDocument document = QJsonDocument::fromJson(data.toUtf8());
+}
 
 void StrategyIO_DB::save(const QString&) {}
 
