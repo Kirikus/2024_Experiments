16c0fdcdb7ec164731dfc76b9155f636699a707f
TheFueR <sukhanov.ge@phystech.edu>
2024-03-22 22:14:12 +0300

    simplify json load realize

--- lib/strategyIO.cpp
+++ lib/strategyIO.cpp
@@ -13,6 +13,21 @@ void StrategyIO_JSON::load(const QString& input_file) {
   file.close();
 
   QJsonDocument document = QJsonDocument::fromJson(data.toUtf8());
+  QJsonObject main_json_object = document.object();
+
+  lib::Variable variable;
+  QString short_name = main_json_object["short_name"].toString();
+  QString full_name = main_json_object["full_name"].toString();
+  QJsonArray json_measurements = main_json_object["measurements"].toArray();
+  for (const auto& j : json_measurements)
+    variable.measurements.push_back(j.toDouble());
+  if (variable.getMeasurementsCount() != 0)
+    // TODO
+    variable.name_full = full_name;
+  variable.name_short = short_name;
+  lib::Manager::getInstance()->deleteVariable();
+  lib::Manager::getInstance()->deleteVariable();
+  lib::Manager::getInstance()->addVariable(variable);
 }
 
 void StrategyIO_DB::save(const QString&) {}
