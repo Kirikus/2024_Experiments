94ca188a645b856402c33db495d257810a9bada4
TheFueR <sukhanov.ge@phystech.edu>
2024-04-17 03:08:28 +0300

    plot settings table refact

--- main/table_models/plot_settings_table.cpp
+++ main/table_models/plot_settings_table.cpp
@@ -3,142 +3,122 @@
 #include "QVariant"
 #include "manager.h"
 
-namespace PlotSettings_shell {
-enum data : int {
-  columnCount = 5,
-  visible = 0,
-  width = 1,
-  point_form = 2,
-  line_type = 3,
-  color = 4
-};
-}  // namespace PlotSettings_shell
+namespace lib {
 
-lib::PlotSettingsTable::PlotSettingsTable(QObject *parent) {}
-
-int lib::PlotSettingsTable::rowCount(const QModelIndex &parent) const {
-  return Manager::getInstance()->getVariablesCount();
+int PlotSettingsTable::rowCount(const QModelIndex &parent) const {
+  return Manager::GetInstance()->GetVariablesCount();
 }
 
-int lib::PlotSettingsTable::columnCount(const QModelIndex &parent) const {
-  return PlotSettings_shell::data::columnCount;
+int PlotSettingsTable::columnCount(const QModelIndex &parent) const {
+  return columns_data::kCount;
 }
 
-QVariant lib::PlotSettingsTable::data(const QModelIndex &index,
-                                      int role) const {
-  int row = index.row();
-  int column = index.column();
-  auto &visual = Manager::getInstance()->getVariable(row).visual;
+QVariant PlotSettingsTable::data(const QModelIndex &index, int role) const {
+  auto &visual = Manager::GetInstance()->GetVariable(index.row()).visual;
   switch (role) {
     case Qt::BackgroundRole:
-      if (column == PlotSettings_shell::data::color) return visual.color;
+      if (index.column() == columns_data::kColor) return visual.color;
     case Qt::CheckStateRole:
-      if (column == PlotSettings_shell::data::visible)
-        return visual.visible ? Qt::Checked : Qt::Unchecked;
-      break;
+      return index.column() == columns_data::kVisible
+                 ? visual.visible ? Qt::Checked : Qt::Unchecked
+                 : QVariant();
     case Qt::DisplayRole:
-      switch (column) {
-        case PlotSettings_shell::data::width:
+      switch (index.column()) {
+        case columns_data::kWidth:
           return visual.width;
-        case PlotSettings_shell::data::point_form:
-          if (visual.point_form == QCPScatterStyle::ScatterShape::ssNone)
-            return QString("Standart");
-          return visual.point_forms.value(visual.point_form);
-        case PlotSettings_shell::data::line_type:
-          return QVariant::fromValue(visual.line_type);
+        case columns_data::kPointShape:
+          return Variable::VisualOptions::point_shapes.value(
+              visual.point_shape);
+        case columns_data::kLineType:
+          return Variable::VisualOptions::line_types.value(visual.line_type);
       }
+    default:
+      return QVariant();
   }
-  return QVariant();
 }
 
-bool lib::PlotSettingsTable::setData(const QModelIndex &index,
-                                     const QVariant &value, int role) {
-  int row = index.row();
-  int column = index.column();
-  auto &visual = Manager::getInstance()->getVariable(row).visual;
+bool PlotSettingsTable::setData(const QModelIndex &index, const QVariant &value,
+                                int role) {
+  auto &visual = Manager::GetInstance()->GetVariable(index.row()).visual;
   switch (role) {
     case Qt::CheckStateRole:
-      if (column == PlotSettings_shell::data::visible) {
+      if (index.column() == columns_data::kVisible) {
         if (!value.canConvert<int>()) return false;
         if (value.toInt() < Qt::Unchecked || value.toInt() > Qt::Checked)
           return false;
         visual.visible =
             static_cast<Qt::CheckState>(value.toInt()) == Qt::Checked;
-        break;
+        return true;
       }
     case Qt::EditRole:
-      switch (column) {
-        case PlotSettings_shell::data::width:
+      switch (index.column()) {
+        case columns_data::kWidth:
           if (!value.toInt()) return false;
           visual.width = value.toInt();
           emit dataChanged(index, index);
           return true;
-        case PlotSettings_shell::data::point_form:
-          visual.point_form =
-              Variable::VisualOptions::point_forms.key(value.toString());
+        case columns_data::kPointShape:
+          visual.point_shape =
+              Variable::VisualOptions::point_shapes.key(value.toString());
           emit dataChanged(index, index);
           return true;
-        case PlotSettings_shell::data::line_type:
+        case columns_data::kLineType:
           visual.line_type =
               Variable::VisualOptions::line_types.key(value.toString());
           emit dataChanged(index, index);
           return true;
-        case PlotSettings_shell::data::color:
+        case columns_data::kColor:
           visual.color = value.value<QColor>();
           emit dataChanged(index, index);
           return true;
       }
+    default:
+      return false;
   }
-  return false;
 }
 
-QVariant lib::PlotSettingsTable::headerData(int section,
-                                            Qt::Orientation orientation,
-                                            int role) const {
-  if (role != Qt::DisplayRole) return QVariant();
-  if (orientation == Qt::Vertical)
-    return QString(
-        Manager::getInstance()->getVariable(section).naming.name_short);
-  switch (section) {
-    case PlotSettings_shell::data::visible:
-      return QString("Visible");
-    case PlotSettings_shell::data::width:
-      return QString("Width");
-    case PlotSettings_shell::data::point_form:
-      return QString("Point form");
-    case PlotSettings_shell::data::line_type:
-      return QString("Line type");
-    case PlotSettings_shell::data::color:
-      return QString("Color");
+QVariant PlotSettingsTable::headerData(int section, Qt::Orientation orientation,
+                                       int role) const {
+  switch (role) {
+    case Qt::DisplayRole:
+      switch (orientation) {
+        case Qt::Vertical:
+          return QString(
+              Manager::GetInstance()->GetVariable(section).naming.title);
+        case Qt::Horizontal:
+          switch (section) {
+            case columns_data::kVisible:
+              return QString("Visible");
+            case columns_data::kWidth:
+              return QString("Width");
+            case columns_data::kPointShape:
+              return QString("Point shape");
+            case columns_data::kLineType:
+              return QString("Line type");
+            case columns_data::kColor:
+              return QString("Color");
+          }
+      }
+    default:
+      return QVariant();
   }
-  return QVariant();
 }
 
 Qt::ItemFlags lib::PlotSettingsTable::flags(const QModelIndex &index) const {
-  int column = index.column();
-  switch (column) {
-    case PlotSettings_shell::data::visible:
-      return Qt::ItemIsEnabled | Qt::ItemIsUserCheckable |
-             QAbstractItemModel::flags(index);
-    case PlotSettings_shell::data::width:
-    case PlotSettings_shell::data::point_form:
-    case PlotSettings_shell::data::line_type:
-    case PlotSettings_shell::data::color:
-      return Qt::ItemIsEditable | QAbstractItemModel::flags(index);
-  }
-  return Qt::ItemIsEditable;
+  return index.column() == columns_data::kVisible
+             ? Qt::ItemIsEnabled | Qt::ItemIsUserCheckable |
+                   QAbstractItemModel::flags(index)
+             : Qt::ItemIsEditable | QAbstractItemModel::flags(index);
 }
 
-bool lib::PlotSettingsTable::insertRows(int row, int count,
-                                        const QModelIndex &parent) {
-  beginInsertRows(parent, row, row + count - 1);
+void PlotSettingsTable::insertRow(int index) {
+  beginInsertRows(QModelIndex(), index, index);
   endInsertRows();
-  return true;
 }
 
-bool lib::PlotSettingsTable::removeRows(int row, int count,
-                                        const QModelIndex &parent) {
-  beginRemoveRows(parent, row, row + count - 1);
+void PlotSettingsTable::removeRow(int index) {
+  beginRemoveRows(QModelIndex(), index, index);
   endRemoveRows();
-  return true;
 }
+
+}  // namespace lib
