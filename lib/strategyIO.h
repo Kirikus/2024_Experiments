b6bc2a827dcc7ef95a0477c94ec8ed87086cf002
TheFueR <sukhanov.ge@phystech.edu>
2024-04-17 03:07:13 +0300

    strategyIO refact

--- lib/strategyIO.h
+++ lib/strategyIO.h
@@ -3,24 +3,28 @@
 
 #include "manager.h"
 
+namespace lib {
+
 struct StrategyIO {
-  virtual void save(const QString&) = 0;
-  virtual void load(const QString&) = 0;
+  virtual void Save(const QString&) = 0;
+  virtual void Load(const QString&) = 0;
 };
 
 struct StrategyIO_JSON : public StrategyIO {
-  void save(const QString&) override;
-  void load(const QString&) override;
-};
-
-struct StrategyIO_DB : public StrategyIO {
-  void save(const QString&) override;
-  void load(const QString&) override;
+  void Save(const QString&) override;
+  void Load(const QString&) override;
 };
 
 struct StrategyIO_CSV : public StrategyIO {
-  void save(const QString&) override;
-  void load(const QString&) override;
+  void Save(const QString&) override;
+  void Load(const QString&) override;
 };
+
+struct StrategyIO_DB : public StrategyIO {
+  void Save(const QString&) override;
+  void Load(const QString&) override;
+};
+
+}  // namespace lib
 
 #endif  // STRATEGYIO_H
