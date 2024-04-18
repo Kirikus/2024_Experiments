b5613aa12b91b2efd3285b809ee7e4b3c2c3457f
TheFueR <sukhanov.ge@phystech.edu>
2024-04-18 17:50:25 +0300

    plot block realized

--- main/manager_odf/block/plot_block.cpp
+++ main/manager_odf/block/plot_block.cpp
@@ -2,20 +2,38 @@
 
 PlotBlock::PlotBlock(QVBoxLayout *block_chain, QPixmap pixmap)
     : Block(), label(new QLabel), pixmap(new QPixmap(pixmap)) {
+  label->setFixedSize(256, 256);
   label->setPixmap(pixmap);
   layout->addWidget(label);
   block_chain->addLayout(layout);
-  label->show();
 }
 
 PlotBlock::~PlotBlock() { layout->removeWidget(label); }
 
-void PlotBlock::Save(QTextCursor *) {}
+void PlotBlock::Save(QTextCursor *cursor) {
+  QImage image = pixmap->toImage();
+  cursor->document()->addResource(QTextDocument::ImageResource, QUrl("plot"),
+                                  image);
+  QTextImageFormat format;
+  format.setQuality(100);
+  format.setName("plot");
+
+  cursor->insertImage(image);
+  cursor->insertBlock();
+}
 
 void PlotBlock::RemoveFromBlockChain(QVBoxLayout *block_chain) {
   block_chain->removeItem(layout);
+  label->hide();
+  deleteBtn->hide();
+  upBtn->hide();
+  downBtn->hide();
 }
 
 void PlotBlock::AddToBlockChain(QVBoxLayout *black_chain, int position) {
   black_chain->insertItem(position, layout);
+  label->show();
+  deleteBtn->show();
+  upBtn->show();
+  downBtn->show();
 }
