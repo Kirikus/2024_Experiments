778fade4ca994c40eb531e74f4148a65a11ed59b
TheFueR <sukhanov.ge@phystech.edu>
2024-04-18 04:49:24 +0300

    added table block files

---
+++ main/manager_odf/block/table_block.cpp
@@ -0,0 +1,23 @@
+#include "table_block.h"
+
+TableBlock::TableBlock(QVBoxLayout *) {}
+
+TableBlock::~TableBlock()
+{
+
+}
+
+void TableBlock::Save(QTextCursor *)
+{
+
+}
+
+void TableBlock::RemoveFromBlockChain(QVBoxLayout *)
+{
+
+}
+
+void TableBlock::AddToBlockChain(QVBoxLayout *, int position)
+{
+
+}
