0ee8baaa566d01c886dc2a027199e8e95bbedae3
MathewKurchenko <mathew.kurchenko23@gmail.com>
2024-04-24 21:46:11 +0300

    histogram v1

--- main/plot_models/histogram.cpp
+++ main/plot_models/histogram.cpp
@@ -6,27 +6,34 @@ void Histogram::Draw(QCustomPlot* plot) {
     plot->clearPlottables();
 
     plot->legend->setVisible(true);
-    int n = lib::Manager::GetInstance()->GetVariablesCount();
 
-    for (int i = 0; i < n; ++i) {
-        const lib::Variable& variable = lib::Manager::GetInstance()->GetVariable(i);
-        if (!variable.visual.visible) continue;
+    const lib::Variable& variable = lib::Manager::GetInstance()->GetVariable(0);
 
-        QVector<double> xAxis_data;
-        QVector<double> yAxis_data;
-        for (int j = 0; j < variable.GetMeasurementsCount(); j++) {
-            if (variable.measurements[j]) {
-                xAxis_data.push_back((j + 0.55 + i * (0.9 / n) + 0.9 / (2 * n)));
-                yAxis_data.push_back(variable.measurements[j]);
+    double max_value = 0;
+
+    for (int j = 0; j < variable.GetMeasurementsCount(); j++) {
+        max_value = std::max(max_value, variable.measurements[j]);
+    }
+
+    QVector<double> xAxis_data;
+    QVector<double> yAxis_data;
+
+    for (int i = 0; i<=max_value; ++i) {
+        int count = 0;
+        for (int j = 0; j<variable.GetMeasurementsCount(); ++j) {
+            if (i <= variable.measurements[j] && variable.measurements[j] < i+1) {
+                count++;
             }
         }
-
-        QCPBars* bar = new QCPBars(plot->xAxis, plot->yAxis);  // нет delete
-
-        bar->setWidth(0.9 / n);
-        bar->setData(xAxis_data, yAxis_data);
-        bar->setBrush(QBrush(variable.visual.color));
+        xAxis_data.push_back(i+0.5);
+        yAxis_data.push_back(count);
     }
+
+    QCPBars* bar = new QCPBars(plot->xAxis, plot->yAxis);
+
+    bar->setWidth(1);
+    bar->setData(xAxis_data, yAxis_data);
+    bar->setBrush(QBrush(variable.visual.color));
 
     plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
     plot->replot();
