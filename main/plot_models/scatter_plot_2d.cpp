8c93e000407c676ec34d7f0b22caab6dd9f47556
MathewKurchenko <mathew.kurchenko23@gmail.com>
2024-04-28 19:39:27 +0300

    remake scatter plot 2d

--- main/plot_models/scatter_plot_2d.cpp
+++ main/plot_models/scatter_plot_2d.cpp
@@ -4,39 +4,31 @@
 
 void ScatterPlot2D::Draw(QCustomPlot* plot, int x, int y) {
   plot->clearGraphs();
-  plot->legend->setVisible(true);
 
-  QVector<int> var{x, y};
+  const lib::Variable& variable_x = lib::Manager::GetInstance()->GetVariable(x);
+  QCPGraph* graph = plot->addGraph();
 
-  for (int i = 0; i < 2; ++i) {
-    const lib::Variable& variable =
-        lib::Manager::GetInstance()->GetVariable(var[i]);
-    QCPGraph* graph = plot->addGraph();
-    graph->setName(variable.naming.title);
-    plot->setFont(QFont("Helvetica", 9));
+  plot->setFont(QFont("Helvetica", 9));
 
-    graph->setLineStyle(QCPGraph::lsNone);
+  graph->setLineStyle(QCPGraph::lsNone);
 
-    graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle));
-    graph->setPen(QPen(QBrush(variable.visual.color), variable.visual.width,
-                       variable.visual.line_type));
+  graph->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle));
+  graph->setPen(QPen(QBrush(variable_x.visual.color), variable_x.visual.width,
+                     variable_x.visual.line_type));
 
-    QVector<double> xAxis_data;
-    QVector<double> yAxis_data;
-    for (int j = 0; j < variable.GetMeasurementsCount(); j++) {
-      if (variable.measurements[j]) {
-        if (i == 1) {
-          xAxis_data.push_back(variable.measurements[j]);
-          yAxis_data.push_back(j + 1);
+  QVector<double> xAxis_data;
+  QVector<double> yAxis_data;
 
-        } else {
-          xAxis_data.push_back(j + 1);
-          yAxis_data.push_back(variable.measurements[j]);
-        }
-      }
-    }
-    graph->setData(xAxis_data, yAxis_data);
+  const lib::Variable& variable_y = lib::Manager::GetInstance()->GetVariable(y);
+
+  for (int j = 0; j < variable_x.GetMeasurementsCount(); j++) {
+    xAxis_data.push_back(variable_x.measurements[j]);
+    yAxis_data.push_back(variable_y.measurements[j]);
   }
+  plot->xAxis->setLabel("Axis " + variable_x.naming.title);
+  plot->yAxis->setLabel("Axis " + variable_y.naming.title);
+  graph->setData(xAxis_data, yAxis_data);
+
   plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
   plot->replot();
 }
